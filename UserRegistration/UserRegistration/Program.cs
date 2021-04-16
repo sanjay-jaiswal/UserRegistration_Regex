a1b1c558e2eb0de5374aeca76d393b0030e1e83e
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-16 23:27:21 +0530

    [Sanju] ADD : Validate last name

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -8,6 +8,10 @@ namespace UserRegistration
         static string firstName = "^[A-Z]{1}[a-z]{2,}$";
         Regex regexName = new Regex(firstName);
 
+        /// <summary>
+        /// Validate first name
+        /// </summary>
+        /// <param name="firstName"></param>
         public void firstNameValidate(string firstName)
         {
             if (regexName.IsMatch(firstName))
@@ -19,13 +23,38 @@ namespace UserRegistration
                 Console.WriteLine("First name is Invalid");
             }
         }
+
+        /// <summary>
+        /// Validate last name
+        /// </summary>
+        /// <param name="lastName"></param>
+        public void lastNameValidate(string lastName)
+        {
+            if (regexName.IsMatch(lastName))
+            {
+                Console.WriteLine("Last name is Valid.");
+            }
+            else
+            {
+                Console.WriteLine("Last name is Invalid.");
+            }
+
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("===============================Welcome to User Registration================================");
             Program pg = new Program();
+
+            //Taking input from user for first name
             Console.WriteLine("Enter Your First Name : ");
             string firstName = Console.ReadLine();
-            pg.firstNameValidate(firstName);  
+            pg.firstNameValidate(firstName);
+
+            //Taking input from user for last name
+            Console.WriteLine("Enter your last name : ");
+            string lastName = Console.ReadLine();
+            pg.lastNameValidate(lastName);
         }
     }
  }
