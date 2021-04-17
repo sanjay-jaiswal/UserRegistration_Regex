4fac5d1538014826bc556e0748d018ce97bbacc7
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-17 22:02:20 +0530

    [Sanju] ADD : Password contains atleast 1 Upper case

--- UserRegistration/UserRegistration/PatternValidator.cs
+++ UserRegistration/UserRegistration/PatternValidator.cs
@@ -10,7 +10,7 @@ namespace UserRegistration
             static string NAME_PATTERN = "^[A-Z][a-z]{2,}$";
             static string EMAIL_PATTERN = "^[0-9a-zA-z]+([._+-][0-9a-zA-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$";
             static string MOBILE_PATTERN = "^[0-9]{2}[ ][0-9]{10}$";
-            static string PASSWORD_PATTERN = "^[A-ZA-z]{8,}$";
+            static string PASSWORD_PATTERN = "[A-Z]{1}[A-Za-z]{7,}";
 
             Regex regexName = new Regex(NAME_PATTERN);
             Regex regEmail = new Regex(EMAIL_PATTERN);
